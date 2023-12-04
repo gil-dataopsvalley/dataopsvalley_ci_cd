fd596a9fd279b8ebbf32fae8ae45746f1411dd93
Gil Kal <gilc86@gmail.com>
2023-12-04 13:26:30 +0200

    no message

--- dbt_ci_cd/create_docker_dgt_govil_dbt.sh
+++ dbt_ci_cd/create_docker_dgt_govil_dbt.sh
@@ -2,27 +2,29 @@
 # Gcloud #
 # gcloud variables
 export DiretoryRepo=dataopsvalley_airflow_k8_dbt
+echo $DiretoryRepo
 # get current project name google-cloud
 export ProjectNameGCP=$(gcloud config get-value project) #Example: dgt-gcp-egov-test-govilbi-0
+echo $ProjectNameGCP
 # name projects exists
 export Test_ProjectNameGCP_dataopsvalley=project-dataopsvalley-test
 export Prod_ProjectNameGCP_dataopsvalley=project-dataopsvalley-prod # still does not exist
 # get user name google-cloud
 export userName=$(gcloud config list account --format "value(core.account)")
 export userName=$(cut -d "@" -f1 <<< "$userName")
-echo $userName
+echo "the user name exec is: "$userName
 
 
 #create folder projects in terminal if not exsit
 mkdir -p projects
-cd projects/
+cd /home/$userName/projects/
 
-# Check the $DIRECTORY_REPO is exists and delete Directory
+# Check the $DiretoryRepo is exists and delete Directory
 echo Path: "$PWD"
-if [ -d "$DIRECTORY_REPO" ]; then
-  echo "$DIRECTORY_REPO does exist."
+if [ -d "$DiretoryRepo" ]; then
+  echo "$DiretoryRepo does exist."
    rm -rf $DiretoryRepo 
-  echo "$DIRECTORY_REPO as deleted."
+  echo "$DiretoryRepo as deleted."
 fi
 
 # Git #
@@ -32,9 +34,9 @@ export GitUser=gil-dataopsvalley
 # git - clone
 git clone https://$GitDomain/$GitUser/$DiretoryRepo.git
 echo "clone success - https://$GitDomain/$GitUser/$DiretoryRepo.git"
-cd $DIRECTORY_REPO/dbt/
+cd $DiretoryRepo/dbt/
 # git get version #
-cd /home/$userName/projects/$DIRECTORY_REPO
+cd /home/$userName/projects/$DiretoryRepo
 export TagVersion=$(git describe --tags --abbrev=0)
 echo 
 echo $TagVersion
@@ -58,7 +60,7 @@ echo "creator: Gil Kal"
 echo $ProjectNameGCP
 echo $Test_ProjectNameGCP_dataopsvalley
 echo $Prod_ProjectNameGCP
-echo DIRECTORY_REPO is: $DIRECTORY_REPO
+echo DiretoryRepo is: $DiretoryRepo
 
 # Check name $ProjectNameGCP and conig environment Prod or Test
 case $ProjectNameGCP in
@@ -86,25 +88,26 @@ echo "The project name " $ProjectNameGCP # " and composer name: " $composer_envi
 #GCS
 # export dag_config_name=config_dgt_airflow_k8_dbt.json
 # echo copy Dag file to gcs composer:  $gcs_composer
-# gsutil cp -r /home/$userName/projects/$DIRECTORY_REPO/EnvDags/$test_composer_environmentName/dags/* gs://$gcs_composer/dags/
+# gsutil cp -r /home/$userName/projects/$DiretoryRepo/EnvDags/$test_composer_environmentName/dags/* gs://$gcs_composer/dags/
 
 # echo copy config json Dag file to gcs
 # export tmp=$(mktemp)
-#jq '."TagVersion" = "'"$TagVersion"'"' /home/$userName/projects/$DIRECTORY_REPO/EnvDags/$test_composer_environmentName/dags/$dag_config_name > "$tmp" && mv "$tmp" /home/$userName/projects/$DIRECTORY_REPO/$test_composer_environmentName/dags/$dag_config_name
-# jq '."artifact_registry" = "'"$artifact_registry"'"' /home/$userName/projects/$DIRECTORY_REPO/EnvDags/$test_composer_environmentName/dags/$dag_config_name > "$tmp" && mv "$tmp" /home/$userName/projects/$DIRECTORY_REPO/$test_composer_environmentName/dags/$dag_config_name
-# jq '."Dbt_project_Name" = "'"$Dbt_project_Name"'"' /home/$userName/projects/$DIRECTORY_REPO/EnvDags/$test_composer_environmentName/dags/$dag_config_name > "$tmp" && mv "$tmp" /home/$userName/projects/$DIRECTORY_REPO/$test_composer_environmentName/dags/$dag_config_name
-# gsutil cp /home/$userName/projects/$DIRECTORY_REPO/EnvDags/$test_composer_environmentName/dags/$dag_config_name gs://$gcs_composer/dags/
+#jq '."TagVersion" = "'"$TagVersion"'"' /home/$userName/projects/$DiretoryRepo/EnvDags/$test_composer_environmentName/dags/$dag_config_name > "$tmp" && mv "$tmp" /home/$userName/projects/$DiretoryRepo/$test_composer_environmentName/dags/$dag_config_name
+# jq '."artifact_registry" = "'"$artifact_registry"'"' /home/$userName/projects/$DiretoryRepo/EnvDags/$test_composer_environmentName/dags/$dag_config_name > "$tmp" && mv "$tmp" /home/$userName/projects/$DiretoryRepo/$test_composer_environmentName/dags/$dag_config_name
+# jq '."Dbt_project_Name" = "'"$Dbt_project_Name"'"' /home/$userName/projects/$DiretoryRepo/EnvDags/$test_composer_environmentName/dags/$dag_config_name > "$tmp" && mv "$tmp" /home/$userName/projects/$DiretoryRepo/$test_composer_environmentName/dags/$dag_config_name
+# gsutil cp /home/$userName/projects/$DiretoryRepo/EnvDags/$test_composer_environmentName/dags/$dag_config_name gs://$gcs_composer/dags/
 ################################################################################
 
 
 # me-west1-docker.pkg.dev/project-dataopsvalley-test/dataopsvalley-docker-dbt-repo
+# me-west1-docker.pkg.dev/project-dataopsvalley-test/dataopsvalley-docker-dbt-repo/dataopsvalley_airflow_k8_dbt
 echo Path: "$PWD"
 
 # Docker #
 docker images
 # docker build
 docker build . -f ./dbt/Dockerfile -t $ArtifactRegistry/$ProjectNameGCP/$ArtifactRegistryRepo/$DbtProjectName:latest
-echo docker build success from git repo: $DIRECTORY_REPO
+echo docker build success from git repo: $DiretoryRepo
 docker images
 # docker tag
 docker tag \
